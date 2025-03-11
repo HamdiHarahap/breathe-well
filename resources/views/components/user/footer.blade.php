f3e726f230bf915bc4dbbcdd9bfdb9fff89b036a
HamdiHarahap <hamdiharahap2005@gmail.com>
2025-03-11 13:23:34 +0700

    commit

--- resources/views/components/user/footer.blade.php
+++ resources/views/components/user/footer.blade.php
@@ -1,14 +1,47 @@
-<footer class="bg-[#215aa9] px-44 text-white py-20 flex gap-12">
-    <div class="flex flex-col gap-2">
-        <h3 class="text-xl font-semibold">BreatheWell</h3>
-        <p class="w-96 text-justify">BreatheWell berkomitmen memberikan layanan kesehatan yang mudah diakses dan terpercaya. Kami membantu masyarakat mengenali gejala dini dan menjaga kesehatan dengan lebih baik.</p>
+<footer class="bg-[#215aa9] px-44 text-white pt-20 pb-8 flex flex-col gap-16">
+    <div class="flex justify-between w-full">
+        <div class="flex flex-col gap-2">
+            <h3 class="text-xl font-semibold">BreatheWell</h3>
+            <p class="w-96 text-justify">BreatheWell berkomitmen memberikan layanan kesehatan yang mudah diakses dan terpercaya. Kami membantu masyarakat mengenali gejala dini dan menjaga kesehatan dengan lebih baik.</p>
+        </div>
+        <div class="flex flex-col gap-2">
+            <h3 class="font-semibold text-lg">Kontak Kami</h3>
+            <div class="flex flex-col gap-1">
+                <div class="flex items-center gap-3">
+                    <img src="{{ asset('/assets/icons/whatsapp.svg') }}" alt="" class="w-6">
+                    <p>+62 812 6036 6275</p>
+                </div>
+                <div class="flex items-center gap-3">
+                    <img src="{{ asset('/assets/icons/gmail.svg') }}" alt="" class="w-6">
+                    <p>breathewell@gmail.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col gap-2">
+            <h3 class="font-semibold text-lg">Links</h3>
+            <ul>
+                <li class="list-disc list-inside"><a href="" class="hover:text-gray-300">Beranda</a></li>
+                <li class="list-disc list-inside"><a href="" class="hover:text-gray-300">Cek Kesehatan</a></li>
+                <li class="list-disc list-inside"><a href="" class="hover:text-gray-300">Tentang Kami</a></li>
+            </ul>
+        </div>
+        <div class="flex flex-col gap-2">
+            <h3 class="font-semibold text-lg">Ikuti Kami</h3>
+            <div class="grid grid-cols-4 gap-3">
+                <div class="border-2 rounded-full p-2 w-fit cursor-pointer hover:scale-110 hover:bg-gray-500">
+                    <img src="{{ asset('/assets/icons/instagram.svg') }}" alt="" class="w-4">
+                </div>
+                <div class="border-2 rounded-full p-2 w-fit cursor-pointer hover:scale-110 hover:bg-gray-500">
+                    <img src="{{ asset('/assets/icons/twitter.svg') }}" alt="" class="w-4">
+                </div>
+                <div class="border-2 rounded-full p-2 w-fit cursor-pointer hover:scale-110 hover:bg-gray-500">
+                    <img src="{{ asset('/assets/icons/youtube.svg') }}" alt="" class="w-4">
+                </div>
+                <div class="border-2 rounded-full p-2 w-fit cursor-pointer hover:scale-110 hover:bg-gray-500">
+                    <img src="{{ asset('/assets/icons/tiktok.svg') }}" alt="" class="w-4">
+                </div>
+            </div>
+        </div>
     </div>
-    <div>
-        <h3 class="font-semibold text-lg">Kontak Kami</h3>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi tempora accusantium non tempore excepturi atque aperiam distinctio enim nemo quos temporibus natus, aliquam rerum sint, quam soluta necessitatibus dignissimos quia?</p>
-    </div>
-    <div>
-        <h3 class="font-semibold text-lg">Links</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus repudiandae, illum ratione autem voluptatibus quasi rem eius perferendis at minima necessitatibus ullam, rerum aperiam mollitia. Inventore id similique perferendis praesentium!</p>
-    </div>
+    <h3 class="font-semibold text-lg text-center">&copy; 2025 BreatheWell. All Rights Reserved</h3>
 </footer>
